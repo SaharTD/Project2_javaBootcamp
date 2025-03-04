fe566845e384bfd90db9d69a7f486fc51770c2dd
Sahar Turki <103968284+SaharTD@users.noreply.github.com>
2025-03-03 21:41:32 -0900

    Update Non_Subscribers.java

--- src/Non_Subscribers.java
+++ src/Non_Subscribers.java
@@ -1,47 +1,63 @@
-public class Non_Subscribers extends Passenger{
+import java.util.ArrayList;
 
-private boolean discount;
+public class Non_Subscribers extends Passenger {
 
-    public Non_Subscribers(Car car, String id, String name,boolean discount) {
-        super(car, id, name);
-        this.discount=discount;
+    private boolean discount;
+
+
+
+
+
+    public Non_Subscribers(Car car, String id, String name, boolean discount) {
+        super( car ,id, name);
+        this.discount = discount;
     }
 
-    public Non_Subscribers(Car car, String id, String name, int tripCost) {
-        super(car, id, name, tripCost);
-    }
 
     @Override
-    double carReserve(Car car)throws Exception  {
+    public void carReserve (Car car) throws Exception {
         try {
-            if(car.getMax_capacity()!=0){
-        if (discount){
-            tripCost= car.fixed_route.getTripPrice()-(car.fixed_route.getTripPrice()*0.1) ;
-            return tripCost;
+            if (car.getMax_capacity() != 0) {
+this.setCar(car);
+car.reduceCapacity();
+this.setTripCost(costCalculations(car));
+                System.out.println("Car reserved successfully for :"+this.getName());
+            } else if (car.getMax_capacity() == 0){
+                this.setCar(null);
+                throw new Exception("\n Note:No capacity available for passenger : "+this.getName());
+            }
 
-        }else {
-            tripCost= car.fixed_route.getTripPrice();
-            return tripCost;
-
-        }
-            } else throw new Exception("\n Note:The maximum capacity of the car was equal to zero ");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
-        return tripCost;
     }
 
-    public void displayInfo() throws Exception {
-        System.out.println("\n Passenger" +
-                "\n Car Code:"+car.getCode()+
-                "\n name :"+name+
-                "\n passenger ID :"+id+
-                "\n Trip Cost :"+carReserve(this.car)+
-                "\n Arrival :"+car.fixed_route.getS_pickupAddress()+
-                "\n Start destination :"+car.fixed_route.getDestination_Address()
-        );
-    }
+    public double costCalculations(Car car) {
+            if (discount) {
+                tripCost = car.fixed_route.getTripPrice() - (car.fixed_route.getTripPrice() * 0.1);
+                return tripCost;
+
+            } else {
+                tripCost = car.fixed_route.getTripPrice();
+                return tripCost;
+
+            }
+        }
+
+        public void displayInfo()  {
+            if(this.car!=null) {
+                System.out.println("\n Passenger" +
+                        "\n Reversed Car information :" + this.getCar() +
+                        "\n name :" + name +
+                        "\n passenger ID :" + id +
+                        "\n Trip Final Cost :" + this.tripCost +
+                        "\n Arrival :" + car.fixed_route.getS_pickupAddress() +
+                        "\n Start destination :" + car.fixed_route.getDestination_Address());
+
+            }else System.out.println(this.getName()+" has no car reservation");
+
+        }
+
 
 
 }
