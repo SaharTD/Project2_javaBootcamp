2ea69702588aa50aa93878cec51888d402f47ee9
Sahar Turki <103968284+SaharTD@users.noreply.github.com>
2025-03-03 21:40:47 -0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,40 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-        Route r1= new Route ("Home","KAFD",88);
-        Route r2= new Route ("Work","Play Ground",50);
+        Route r1= new Route ("EBank","KAFD",88);
+        Route r2= new Route ("Dorm","Play Ground",50);
 
         Car c1 = new Car("2200",r1,0);
-        Car c2 = new Car ("0044",r2,3);
+        Car c2 = new Car ("0044",r2,2);
+
 
 
         ArrayList<Passenger> passengerList =new ArrayList<>();
-        Passenger p1= new Subscribers_P(c1,"2222","Sahar Turki");
-        Passenger p2= new Non_Subscribers(c2,"4444","Osama Turki",true);
+            Passenger p1= new Subscribers_P(c1,"2222","Sahar Turki");
+            Passenger p2= new Non_Subscribers(c2,"4444","Osama Turki",true);
+           Passenger p3= new Non_Subscribers(c2,"4554","Ahmad ",false);
+           Passenger p4= new Non_Subscribers(c2,"4334","Sara ",false);
 
         passengerList.add(p1);
         passengerList.add(p2);
+        passengerList.add(p3);
+        passengerList.add(p4);
+
+
+
 
         for (Passenger passenger : passengerList) {
-            passenger.displayInfo();
-        }
+            try {
+passenger.carReserve(passenger.getCar());
+                passenger.displayInfo();
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            }
+
+
+
 
 
 
